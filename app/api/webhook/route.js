143c58873fea691e09768c48e70c09eee4b5a192
GIANT <isiri.sriuthai@gmail.com>
2023-11-25 20:31:30 +0700

    deployable i guess

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -1,37 +1,63 @@
-const crypto = require("crypto");
-require('dotenv').config();
+const crypto = require("crypto")
+require('dotenv').config()
+import { Client } from '@line/bot-sdk'
+import { headers } from 'next/headers'
+
+const client = new Client({
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET
+});
 
 const channelSecret = process.env.LINE_CHANNEL_SECRET; // Channel secret string
 
-export const POST = async (req, res) => {
-    try {
-        const xLineSignature = req.headers['x-line-signature']; // Access the x-line-signature header
-        const requestBody = req.body; // In Next.js, the request body is already parsed
-        
-        // Calculate the expected signature
-        const signature = crypto
-          .createHmac("SHA256", channelSecret)
-          .update(JSON.stringify(requestBody))
-          .digest("base64");
-        
-        if (signature !== xLineSignature) {
-          // Request is not valid
-          return res.status(403).json({ error: 'Forbidden' });
-        }
+export const POST = async (req) => {
+    const headersList = headers()
+    const body = await req.json()
+    // x-line-signature verification
+    const xLineSignature = headersList.get('x-line-signature');
+    const signature = crypto
+      .createHmac("SHA256", channelSecret)
+      .update(JSON.stringify(body))
+      .digest("base64");
+    console.log(body)
 
-        // Your reply logic goes here
-        for (const event of requestBody.events) {
-            if (event.type === 'message' && event.message.type === 'text') {
-                const replyToken = event.replyToken;
-                const message = { type: 'text', text: 'Hello' };
-                await client.replyMessage(replyToken, message);
-            }
-        }
-
-        // Send a 200 OK response
-        res.status(200).json({ message: 'Success' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    if (signature !== xLineSignature) {
+        return new Response(JSON.stringify({ error: 'Unauthorized' }), {
+            status: 401,
+        });
     }
-  }
+
+    // Check if events array exists and handle each event
+    if (body.events && Array.isArray(body.events)) {
+        try {
+        const results = await Promise.all(body.events.map(handleEvent));
+        return new Response(JSON.stringify(results), {
+            status: 200,
+        });
+        } catch (err) {
+        console.error(err);
+        return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
+            status: 500,
+        });
+        }
+    } else {
+        // No events found in the webhook object
+        return new Response(JSON.stringify({ error: 'No events found' }), {
+        status: 400,
+        });
+    }
+};
+  
+// Handling each event
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        // ignore non-text-message event
+        return Promise.resolve(null);
+    }
+    // Echo text message
+    return client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: event.message.text
+    });
+}
+  
