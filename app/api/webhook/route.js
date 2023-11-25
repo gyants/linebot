c3170f93efbdce367405c10205022936f5f8999a
GIANT <isiri.sriuthai@gmail.com>
2023-11-25 19:47:28 +0700

    fix return status method

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -3,40 +3,35 @@ require('dotenv').config();
 
 const channelSecret = process.env.LINE_CHANNEL_SECRET; // Channel secret string
 
-export const POST = async (req, res) => {
+export default async function handler(req, res) {
     try {
-      const xLineSignature = req.headers['x-line-signature']; // Get the x-line-signature header from the request
-      const requestBody = await req.json(); // Parse the JSON request body
-      
-      // Calculate the expected signature
-      const signature = crypto
-        .createHmac("SHA256", channelSecret)
-        .update(JSON.stringify(requestBody))
-        .digest("base64");
-      
-      if (signature !== xLineSignature) {
-        // Request is not valid, reject it or handle the error
-        // Send an error response, e.g., 403 Forbidden
-        return res.sendStatus(403);
-      }
-  
-      // Request is valid, proceed with handling it
-  
-      // Your reply logic goes here
-      for (const event of requestBody.events) {
-        if (event.type === 'message' && event.message.type === 'text') {
-          const replyToken = event.replyToken;
-          const message = { type: 'text', text: 'Hello' };
-          await client.replyMessage(replyToken, message);
+        const xLineSignature = req.headers['x-line-signature']; // Access the x-line-signature header
+        const requestBody = req.body; // In Next.js, the request body is already parsed
+        
+        // Calculate the expected signature
+        const signature = crypto
+          .createHmac("SHA256", channelSecret)
+          .update(JSON.stringify(requestBody))
+          .digest("base64");
+        
+        if (signature !== xLineSignature) {
+          // Request is not valid
+          return res.status(403).json({ error: 'Forbidden' });
         }
-      }
-  
-      // Send a 200 OK response to acknowledge receipt of the webhook
-      return res.sendStatus(200);
+
+        // Your reply logic goes here
+        for (const event of requestBody.events) {
+            if (event.type === 'message' && event.message.type === 'text') {
+                const replyToken = event.replyToken;
+                const message = { type: 'text', text: 'Hello' };
+                await client.replyMessage(replyToken, message);
+            }
+        }
+
+        // Send a 200 OK response
+        res.status(200).json({ message: 'Success' });
     } catch (error) {
-      console.error(error);
-      // Handle any errors that may occur during request processing
-      // Send an appropriate error response
-      return res.status(500).json({ error: 'Something went wrong' });
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-  }
+}
